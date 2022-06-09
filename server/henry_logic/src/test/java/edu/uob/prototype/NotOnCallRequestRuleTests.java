02e2e96d55052c5886bc413df3d0ab0fd5da119c
Henry <henry_letton@hotmail.com>
2022-06-09 14:49:57 +0100

    Added rule
    Rule for enforcing not on call requests

--- server/henry_logic/src/test/java/edu/uob/prototype/NotOnCallRequestRuleTests.java
+++ server/henry_logic/src/test/java/edu/uob/prototype/NotOnCallRequestRuleTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-final class AnnualLeaveRuleTests {
+final class NotOnCallRequestRuleTests {
 
     @Test
     void testBasicShiftsAndRules() {
@@ -14,20 +14,20 @@ final class AnnualLeaveRuleTests {
         Shift s3 = new Shift(ShiftTypes.DayOnCall);
         Shift s4 = new Shift(ShiftTypes.NormalShift);
         // Not working day
-        Rule r1 = new AnnualLeaveRule(s1);
-        assertFalse(r1.isBroken(), "Not working should not break annual leave rule");
+        Rule r1 = new NotOnCallRequestRule(s1);
+        assertFalse(r1.isBroken(), "Not working should not break request for leave rule");
         assertEquals(0, r1.getCost(), "0 cost expected");
         // Night on call
-        Rule r2 = new AnnualLeaveRule(s2);
-        assertTrue(r2.isBroken(), "On call night shift should break annual leave rule");
+        Rule r2 = new NotOnCallRequestRule(s2);
+        assertTrue(r2.isBroken(), "On call night shift should break request for leave rule");
         assertTrue(r2.getCost()>0, ">0 cost expected");
         // Night on call
-        Rule r3 = new AnnualLeaveRule(s3);
-        assertTrue(r3.isBroken(), "On call day shift should break annual leave rule");
+        Rule r3 = new NotOnCallRequestRule(s3);
+        assertTrue(r3.isBroken(), "On call day shift should break request for leave rule");
         assertTrue(r3.getCost()>0, ">0 cost expected");
         // Night on call
-        Rule r4 = new AnnualLeaveRule(s4);
-        assertTrue(r4.isBroken(), "Normal shift should break annual leave rule");
+        Rule r4 = new NotOnCallRequestRule(s4);
+        assertTrue(r4.isBroken(), "Normal shift should break request for leave rule");
         assertTrue(r4.getCost()>0, ">0 cost expected");
     }
 }

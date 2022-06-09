9af0156b582c5a7d0a1c096527735e952a25aecd
Henry <henry_letton@hotmail.com>
2022-06-09 11:14:32 +0100

    Fix rule
    Enforce 48 hours off not just next day

--- server/henry_logic/src/main/java/edu/uob/prototype/Max7ShiftsRule.java
+++ server/henry_logic/src/main/java/edu/uob/prototype/Max7ShiftsRule.java
@@ -8,8 +8,8 @@ public class Max7ShiftsRule extends Rule {
     private final HashMap<String, Shift> shifts;
     private boolean broken;
 
-    public Max7ShiftsRule(Shift day1, Shift day2, Shift day3, Shift day4,
-                              Shift day5, Shift day6, Shift day7, Shift day8) {
+    public Max7ShiftsRule(Shift day1, Shift day2, Shift day3, Shift day4, Shift day5,
+                          Shift day6, Shift day7, Shift day8, Shift day9, Shift day10) {
         shifts = new HashMap<>();
         shifts.put("day1", day1);
         shifts.put("day2", day2);
@@ -19,6 +19,8 @@ public class Max7ShiftsRule extends Rule {
         shifts.put("day6", day6);
         shifts.put("day7", day7);
         shifts.put("day8", day8);
+        shifts.put("day9", day9);
+        shifts.put("day10", day10);
         refresh();
     }
 
@@ -41,11 +43,7 @@ public class Max7ShiftsRule extends Rule {
             broken = false;
             return;
         }
-        broken = !isDay8NotWorking();
-    }
-
-    private boolean isDay8NotWorking() {
-        return shifts.get("day8").getType().equals(ShiftTypes.NotWorking);
+        broken = !have48HoursOff();
     }
 
     private boolean areDays1To7Working() {
@@ -53,4 +51,23 @@ public class Max7ShiftsRule extends Rule {
                 shifts.get("day3").isWorking() && shifts.get("day4").isWorking() &&
                 shifts.get("day5").isWorking() && shifts.get("day6").isWorking() && shifts.get("day7").isWorking();
     }
+
+    private boolean haveTwoDaysOff() {
+        return shifts.get("day8").getHours() + shifts.get("day9").getHours() == 0;
+    }
+
+    private boolean have48HoursOff() {
+        if (shifts.get("day7").getType().equals(ShiftTypes.NightOnCall)) {
+            return isDay10NightOrNotWorking() && haveTwoDaysOff();
+        }
+        return haveTwoDaysOff();
+    }
+
+    private boolean isDay10NightOrNotWorking() {
+        return switch (shifts.get("day10").getType()) {
+            case NightOnCall, NotWorking -> true;
+            default -> false;
+        };
+    }
+
 }
--- server/henry_logic/src/test/java/edu/uob/prototype/Max7ShiftsRuleTest.java
+++ server/henry_logic/src/test/java/edu/uob/prototype/Max7ShiftsRuleTest.java
@@ -13,19 +13,19 @@ public class Max7ShiftsRuleTest {
         Shift s3 = new Shift(ShiftTypes.NightOnCall);
         Shift s4 = new Shift(ShiftTypes.DayOnCall);
         // Non-working week, rule should be met
-        Rule r1 = new Max7ShiftsRule(s1, s1, s1, s1, s1, s1, s1, s1);
+        Rule r1 = new Max7ShiftsRule(s1, s1, s1, s1, s1, s1, s1, s1, s1, s1);
         assertFalse(r1.isBroken(), "No working, rule should be met");
         assertEquals(0, r1.getCost(), "0 cost expected");
-        // 7 days of work then not working day, rule should be met
-        Rule r2 = new Max7ShiftsRule(s2, s2, s2, s2, s2, s2, s2, s1);
+        // 7 days of work then not working, rule should be met
+        Rule r2 = new Max7ShiftsRule(s2, s2, s2, s2, s2, s2, s2, s1, s1, s1);
         assertFalse(r2.isBroken(), "0 hour week should meet rule");
         assertEquals(0, r2.getCost(), "0 cost expected");
         // 8 days of work, rule should not be met
-        Rule r3 = new Max7ShiftsRule(s3, s3, s3, s3, s3, s3, s3, s3);
+        Rule r3 = new Max7ShiftsRule(s3, s3, s3, s3, s3, s3, s3, s3, s1, s1);
         assertTrue(r3.isBroken(), "8 nights shifts should break rule");
         assertTrue(r3.getCost()>0, ">0 cost expected");
         // 7 days of work with one not working day in middle, rule should be met
-        Rule r4 = new Max7ShiftsRule(s4, s4, s4, s4, s4, s1, s4, s4);
+        Rule r4 = new Max7ShiftsRule(s4, s4, s4, s4, s4, s1, s4, s4, s1, s1);
         assertFalse(r4.isBroken(), "Rule should be met");
         assertEquals(0, r4.getCost(), "0 cost expected");
     }

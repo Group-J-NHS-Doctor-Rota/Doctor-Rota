fff10dcd3f4ec9585da1a911b26c998f133e97fa
Henry <henry_letton@hotmail.com>
2022-06-09 14:30:13 +0100

    New rule
    New rule for enforcing annual leave

---
+++ server/henry_logic/src/main/java/edu/uob/prototype/AnnualLeaveRule.java
@@ -0,0 +1,31 @@
+package edu.uob.prototype;
+
+public class AnnualLeaveRule extends Rule {
+
+    private final int cost = 50;
+    private final Shift shift;
+    private boolean broken;
+
+    public AnnualLeaveRule(Shift day) {
+        shift = day;
+        refresh();
+    }
+
+    @Override
+    public boolean isBroken() {
+        return broken;
+    }
+
+    @Override
+    public int getCost() {
+        if(broken) {
+            return cost;
+        }
+        return 0;
+    }
+
+    @Override
+    public void refresh() {
+        broken = shift.isWorking();
+    }
+}

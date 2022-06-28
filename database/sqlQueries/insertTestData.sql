96b69ebfdd1853658f70680988313214d5460827
Henry <henry_letton@hotmail.com>
2022-06-28 12:46:29 +0100

    Camel case correction

--- database/sqlQueries/insertTestData.sql
+++ database/sqlQueries/insertTestData.sql
@@ -1,22 +1,22 @@
 /*This data is completely made up*/
 
 /*rotaGroups data*/
-INSERT INTO rotagroups (startdate, enddate, status)
+INSERT INTO rotaGroups (startDate, endDate, status)
 VALUES
     ('2021-08-04', '2021-11-02', true);
 
 /*accounts data*/
-INSERT INTO accounts (name, password, salt, email, doctorid, accountstatus, doctorstatus, level, rotagroupid, timeworked, fixedworking)
+INSERT INTO accounts (name, password, salt, email, doctorId, accountStatus, doctorStatus, level, rotaGroupId, timeWorked, fixedWorking)
 VALUES
     ('John Smith', 'sdafgndfjgn', '123', 'test@test.com', '9876', 0, 0, 1, 1, 1, false);
 
 /*shifts data*/
-INSERT INTO shifts (accountid, rotagroupid, rotatypeid, date, type, rulenotes)
+INSERT INTO shifts (accountId, rotaGroupId, rotaTypeId, date, type, ruleNotes)
 VALUES
     (1, 1, 1, '2021-09-01', 0, '');
 
 /*leaveRequests data*/
-INSERT INTO leaveRequests (accountid, date, type, note, status)
+INSERT INTO leaveRequests (accountId, date, type, note, status)
 VALUES
     (1, '2021-09-07', 0, '', 0);
 

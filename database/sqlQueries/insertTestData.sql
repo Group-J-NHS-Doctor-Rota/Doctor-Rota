8f27c39cf1952ec52ceefc46bd0cb7b3e013e555
Henry <henry_letton@hotmail.com>
2022-06-28 09:35:02 +0100

    Test data
    Added query to insert test data into database tables

---
+++ database/sqlQueries/insertTestData.sql
@@ -0,0 +1,13 @@
+/*This data is completely made up*/
+
+/*rotaGroups data*/
+INSERT INTO rotagroups (startdate, enddate, status)
+VALUES
+    ('2021-08-04', '2021-11-02', true);
+
+/*accounts data*/
+INSERT INTO accounts (name, password, salt, email, doctorid, accountstatus, doctorstatus, level, rotagroupid, timeworked, fixedworking)
+VALUES
+    ('John Smith', 'sdafgndfjgn', '123', 'test@test.com', '9876', 0, 0, 1, 1, 1, false);
+
+

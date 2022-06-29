1132dfe10624c324d6e2bb6e7708af25a7057d1d
Henry <henry_letton@hotmail.com>
2022-06-29 15:56:41 +0100

    Connection test
    Check the database can be reached.

--- server/src/test/java/edu/uob/ConnectionTests.java
+++ server/src/test/java/edu/uob/ConnectionTests.java
@@ -4,12 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class ConnectionTests {
 
     @Test
     void testGetConnectionString() {
         String connectionString = ConnectionTools.getConnectionString();
+        // Check connection sting has been found, so is not empty or null
         assertNotEquals("", connectionString);
         assertNotEquals(null, connectionString);
+    }
+
+    @Test
+    void testConnection() throws SQLException {
+        // Cannot connect with empty or incorrect connection strings
+        assertThrows(SQLException.class, ()->DriverManager.getConnection(null));
+        assertThrows(SQLException.class, ()->DriverManager.getConnection(""));
+        assertThrows(SQLException.class, ()->DriverManager.getConnection("Some/Gibberish"));
+        // Correct connection string should allow for connection with no issue
+        String connectionString = ConnectionTools.getConnectionString();
+        try(Connection c = DriverManager.getConnection(connectionString)) {
+            assertTrue(true, "Database connection should have worked");
+        } catch (SQLException e) {
+            fail("Database connection should have worked");
+        }
     }
 }

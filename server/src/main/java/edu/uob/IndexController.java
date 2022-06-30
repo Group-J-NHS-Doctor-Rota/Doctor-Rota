331e9e4eb3b236a205ac3917c914144980b7fda9
Henry <henry_letton@hotmail.com>
2022-06-30 15:37:13 +0100

    Test server path
    Simple website path to check that spring boot server is running and can connect to the database

--- server/src/main/java/edu/uob/IndexController.java
+++ server/src/main/java/edu/uob/IndexController.java
@@ -1,13 +1,32 @@
 package edu.uob;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello there! I am running.";
+    public ResponseEntity<String> index() {
+        return ResponseEntity.status(HttpStatus.OK).body("Spring boot server running correctly (CODE 200)\n");
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> getTest() {
+        String connectionString = ConnectionTools.getConnectionString();
+        try(Connection c = DriverManager.getConnection(connectionString)) {
+            return ResponseEntity.status(HttpStatus.OK).body("Test ok (CODE 200)\n");
+        } catch (SQLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("The request was not completed due to database connection issues (CODE 500)\n");
+        }
     }
 }

d4b96d0c885a2eab396ce8efcb1aa0ec30b65274
Henry <henry_letton@hotmail.com>
2022-07-12 09:01:59 +0100

    Testing
    Written a test for delete account request. Takes over 10 secinds to run.

---
+++ server/src/test/java/edu/uob/DeleteOperationsTests.java
@@ -0,0 +1,62 @@
+package edu.uob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DeleteOperationsTests {
+
+    @Test
+    void testDeleteAccount() {
+        String connectionString = ConnectionTools.getConnectionString();
+        try(Connection c = DriverManager.getConnection(connectionString)) {
+            // Create new account with id 991999999 (definitely unused)
+            assertFalse(ConnectionTools.accountIdExists(991999999, c));
+            String SQL = "INSERT INTO accounts (id, username, password, salt, email, annualLeave, studyLeave, workingHours, level) " +
+                    "VALUES (991999999, 'Test Person', 'sdfdfsgghndfh', '987', 'person@test.com', 15, 15, 48, 0);";
+            try (PreparedStatement s = c.prepareStatement(SQL)) {
+                s.executeUpdate();
+            }
+            // Add data to various tables
+            PutOperations.putWorkingDays(991999999, true, true, true, true, false, false, false);
+            SQL = "INSERT INTO shifts (accountId, rotaGroupId, rotaTypeId, date, type, ruleNotes) VALUES (991999999, 1, 1, '2022-07-11', 0, ''); " +
+                    "INSERT INTO leaveRequests (accountId, date, type, note, status) VALUES (991999999, '2022-07-12', 0, '', 0); " +
+                    "INSERT INTO accountLeaveRequestRelationships (accountId, leaveRequestId, status) VALUES (991999999, 1, 1); " +
+                    "INSERT INTO accountRotaTypes (accountId, rotaTypeId, rotaGroupId, startDate, endDate) VALUES (991999999, 2, 1, '2022-07-01', '2022-08-01'); " +
+                    "INSERT INTO fixedRotaShifts (accountId, date, shiftType) VALUES (991999999, '2022-07-13', 1);";
+            try (PreparedStatement s = c.prepareStatement(SQL)) {
+                s.executeUpdate();
+            }
+            // Check data has gone in
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "partTimeDetails", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "fixedRotaShifts", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "accountRotaTypes", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "accountLeaveRequestRelationships", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "leaveRequests", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "accountId", "shifts", c));
+            assertTrue(ConnectionTools.idExistInTable(991999999, "id", "accounts", c));
+            // Delete account
+            DeleteOperations.deleteAccount(991999999);
+            // Try to delete account again
+            assertThrows(ResponseStatusException.class, ()-> DeleteOperations.deleteAccount(991999999));
+            // Check data is no longer there
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "partTimeDetails", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "fixedRotaShifts", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "accountRotaTypes", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "accountLeaveRequestRelationships", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "leaveRequests", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "accountId", "shifts", c));
+            assertFalse(ConnectionTools.idExistInTable(991999999, "id", "accounts", c));
+        } catch (SQLException e) {
+            fail("Database connection and SQL queries should have worked\n" + e);
+        }
+    }
+
+}

700f15aaee15c235cabb0b8d00533b49098b9928
Henry <henry_letton@hotmail.com>
2022-07-20 13:57:35 +0100

    Skeleton requests testing
    This test works to check the input parameters and any output. It cannot check functionality yet.

--- server/src/test/java/edu/uob/MySpringApplicationTests.java
+++ server/src/test/java/edu/uob/MySpringApplicationTests.java
@@ -3,16 +3,19 @@ package edu.uob;
 //Some tests adapted from link:
 //https://www.javainuse.com/spring/springboot_testcases
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -44,6 +47,32 @@ public class MySpringApplicationTests {
 	public void testDbConnection() throws Exception {
 		// Check the /test url returns ok, so the server can connect to the database
 		mockMvc.perform(get("/test")).andExpect(status().isOk());
+	}
+
+	//TODO remove this test once skeleton requests have been completed
+	@Test
+	public void testSkeletonRequests() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		// Check rotabuild works without any variables
+		mockMvc.perform(put("/rotabuild")).andExpect(status().isOk());
+		// Check login needs a request parameter and a header variable
+		mockMvc.perform(get("/login?username=asdfasdf").header("password", "myPassword")).andExpect(status().isOk());
+		// Check login returns three parameters in JSON format
+		MvcResult result = mockMvc.perform(get("/login?username=5dgsdf4t").header("password", "myPassword")).andReturn();
+		JsonNode node = mapper.readTree(result.getResponse().getContentAsString());
+		assertTrue(node.has("token"));
+		assertTrue(node.has("accountId"));
+		assertTrue(node.has("level"));
+		// Check password needs a request parameter and two header variables
+		mockMvc.perform(patch("/password?accountId=1").header("oldPassword", "myPassword")
+				.header("newPassword", "myPassword2")).andExpect(status().isOk());
+		// Check password needs two different values for the header variables
+		mockMvc.perform(patch("/password?accountId=2").header("oldPassword", "myPassword")
+				.header("newPassword", "myPassword")).andExpect(status().isConflict());
+		// Check passwordReset needs two request parameters
+		mockMvc.perform(patch("/passwordReset?username=myUsername&email=test@test.com")).andExpect(status().isOk());
+		// Check logout works without any variables
+		mockMvc.perform(patch("/logout")).andExpect(status().isOk());
 	}
 
 }

59115b69ef18c774770a2610f11503f9c9cd4462
Henry <henry_letton@hotmail.com>
2022-07-22 11:33:00 +0100

    Generate account id function
    This function generates a random nmber between 900000000 and 999999999.
    This values will never realistic be used as real account ids in the system.

---
+++ server/src/test/java/edu/uob/TestTools.java
@@ -0,0 +1,17 @@
+package edu.uob;
+
+import org.springframework.boot.SpringApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TestTools {
+
+    public static int getTestAccountId() {
+        //https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+        // Any 9-digit number beginning with 9 is reserved for testing
+        return ThreadLocalRandom.current().nextInt(900000000, 1000000000);
+    }
+
+}

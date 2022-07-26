18166d1a02ba0fcbc4218c5aa1b1a2d09064aa03
Rongkun Wang <wangrongkun@outlook.com>
2022-07-26 02:54:59 +0100

    use BCryptPasswordEncoder

--- server/src/main/java/edu/uob/Encryption.java
+++ server/src/main/java/edu/uob/Encryption.java
@@ -1,72 +1,65 @@
 package edu.uob;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Encryption {
-    private final String pwd;
-    private static final Random RANDOM = new SecureRandom();
-    private static final int ITERATIONS = 10000;
-    private static final int KEY_LENGTH = 256;
+    private BCryptPasswordEncoder encoder;
 
-    public Encryption(String password) {
-        pwd = password;
+    public Encryption() {
+        encoder = new BCryptPasswordEncoder();
     }
 
-//    private byte[] getMD5(String s) throws NoSuchAlgorithmException,
-//            UnsupportedEncodingException {
-//        byte[] bytesOfMessage = s.getBytes("UTF-8");
-//        //    Uses MD5 Crypt as the hash algorithm.
-//        MessageDigest md = MessageDigest.getInstance("MD5");
-//        byte[] theMD5digest = md.digest(bytesOfMessage);
-//        return theMD5digest;
+    /**
+     * To use BCryptPasswordEncoder, you need to add a dependency (spring-boot-starter-security)
+     * BCrypt will store the salt inside the hash value itself
+     * For instance, the following hash value:
+     * $2a$10$ZLhnHxdpHETcxmtEStgpI./Ri1mksgJ9iDP36FmfMdYyVg9g0b2dq
+     * Separates three fields by $:
+     * The “2a” represents the BCrypt algorithm version
+     * The “10” represents the strength of the algorithm
+     * The “ZLhnHxdpHETcxmtEStgpI.” part is actually the randomly generated salt
+     * The remaining part of the last field is the actual hashed version of the plain text
+     * See: https://www.baeldung.com/spring-security-registration-password-encoding-bcrypt
+     * */
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
 //    }
 
-//    https://stackoverflow.com/questions/18142745/how-do-i-generate-a-salt-in-java-for-salted-hash
-    private byte[] getSalt() {
-        // salt should be unique for every user
-        // todo store salt in database
-        byte[] salt = new byte[16];
-        RANDOM.nextBytes(salt);
-        return salt;
+    public String getFinalPwd(String pwd) {
+        return encoder.encode(pwd);
     }
 
-    public static byte[] hash(char[] password, byte[] salt) {
-        PBEKeySpec spec = new PBEKeySpec(password, salt, ITERATIONS, KEY_LENGTH);
-        Arrays.fill(password, Character.MIN_VALUE);
-        try {
-            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-            return skf.generateSecret(spec).getEncoded();
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
-        } finally {
-            spec.clearPassword();
-        }
-    }
+    /**
+     * In the first version of Encryption class,
+     * I wrote our own Spring password encoding scheme based on salts and peppers.
+     * But after doing some research, I found it might be not safe.
+     * You could see the first version here:
+     * https://github.com/Group-J-NHS-Doctor-Rota/Doctor-Rota/commit/66c6ab8e744151466ce719b05590c4fc5960ce77
+     *
+     * In the second version, I used PBKDF2WithHmacSHA1 algorithm to hash password,
+     * and used SecureRandom to generate unique salt.
+     * Reference:
+     * https://stackoverflow.com/questions/18142745/how-do-i-generate-a-salt-in-java-for-salted-hash
+     * The second version:
+     * https://github.com/Group-J-NHS-Doctor-Rota/Doctor-Rota/commit/a0a5ade4c8e62c92accfed4c38663de2eefd59b1
+     *
+     * Recently, I found BCryptPasswordEncoder was simple to use,
+     * and it seems to be more suitable for spring boot.
+     * So I wrote the third version using BCryptPasswordEncoder.
+     * I haven't combined this encoder with spring boot as the tutorial told,
+     * I just wrote a function which could create a secure password using BCryptPasswordEncoder.
+     * */
 
-    public static boolean isExpectedPassword(char[] password, byte[] salt, byte[] expectedHash) {
-        byte[] pwdHash = hash(password, salt);
-        Arrays.fill(password, Character.MIN_VALUE);
-        if (pwdHash.length != expectedHash.length) return false;
-        for (int i = 0; i < pwdHash.length; i++) {
-            if (pwdHash[i] != expectedHash[i]) return false;
-        }
-        return true;
-    }
+    // Some people don't advise using pepper?
+//    https://stackoverflow.com/questions/71813290/how-do-i-implement-a-pepper-in-spring-mvc
+//    https://stackoverflow.com/questions/16891729/best-practices-salting-peppering-passwords
 
-//    public byte[] getFinalPwd() throws UnsupportedEncodingException, NoSuchAlgorithmException {
-//        // todo there would be only one pepper for the entire application
-    // todo do not store pepper in database
-
-//        String pepper = "";
-//        String s = pwd + getSalt();
-//        byte[] finalPwd = getMD5(s);
-//        getMD5(pepper);
-//        return finalPwd;
-//    }
 }

0523f8f7365258ca41e0389dcd9a405e36433462
Rongkun Wang <wangrongkun@outlook.com>
2022-07-28 03:19:43 +0100

    create basic email functions

---
+++ server/src/main/java/edu/uob/Email.java
@@ -0,0 +1,51 @@
+package edu.uob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+public class Email {
+    public Email() {
+        //
+    }
+
+//    https://www.baeldung.com/spring-email
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("my.gmail@gmail.com");
+        mailSender.setPassword("password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+//    @Component
+//    public class EmailServiceImpl implements EmailService {
+//
+//        @Autowired
+//        private JavaMailSender emailSender;
+//
+//        public void sendSimpleMessage(String to, String subject, String text) {
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom("noreply@baeldung.com");
+//            message.setTo(to);
+//            message.setSubject(subject);
+//            message.setText(text);
+//            emailSender.send(message);
+//        }
+//    }
+}
